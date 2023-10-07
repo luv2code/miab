3692659ce4123deabbe4245e8c0d43733952a28b
Matthew Taylor <luv2code@gmail.com>
2023-10-06 19:46:28 -0500

    implemented provisioner

--- go.mod
+++ go.mod
@@ -1,8 +1,38 @@
 module github.com/caddy-dns/template
 
-go 1.16
+go 1.21.0
 
 require (
 	github.com/caddyserver/caddy/v2 v2.4.1
-	github.com/libdns/template v0.1.0
+	github.com/luv2code/libdns-miab v0.0.0-20231007003149-631077eb0975
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/caddyserver/certmagic v0.13.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
+	github.com/libdns/libdns v0.2.1 // indirect
+	github.com/luv2code/gomiabdns v0.0.0-20231004034140-fc94bf3eb2d2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mholt/acmez v0.1.3 // indirect
+	github.com/miekg/dns v1.1.30 // indirect
+	github.com/prometheus/client_golang v1.9.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.15.0 // indirect
+	github.com/prometheus/procfs v0.2.0 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
 )
--- module.go
+++ module.go
@@ -1,15 +1,13 @@
 package template
 
 import (
-	"fmt"
-
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	libdnstemplate "github.com/libdns/template"
+	miab "github.com/luv2code/libdns-miab"
 )
 
 // Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
-type Provider struct{ *libdnstemplate.Provider }
+type Provider struct{ *miab.Provider }
 
 func init() {
 	caddy.RegisterModule(Provider{})
@@ -19,42 +17,62 @@ func init() {
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
 		ID:  "dns.providers.template",
-		New: func() caddy.Module { return &Provider{new(libdnstemplate.Provider)} },
+		New: func() caddy.Module { return &Provider{new(miab.Provider)} },
 	}
 }
 
-// TODO: This is just an example. Useful to allow env variable placeholders; update accordingly.
 // Provision sets up the module. Implements caddy.Provisioner.
 func (p *Provider) Provision(ctx caddy.Context) error {
-	p.Provider.APIToken = caddy.NewReplacer().ReplaceAll(p.Provider.APIToken, "")
-	return fmt.Errorf("TODO: not implemented")
+	p.Provider.APIURL = caddy.NewReplacer().ReplaceAll(p.Provider.APIURL, "")
+	p.Provider.EmailAddress = caddy.NewReplacer().ReplaceAll(p.Provider.EmailAddress, "")
+	p.Provider.Password = caddy.NewReplacer().ReplaceAll(p.Provider.Password, "")
+	return nil
 }
 
-// TODO: This is just an example. Update accordingly.
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-// providername [<api_token>] {
-//     api_token <api_token>
-// }
-//
-// **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
+//	miab {
+//	    api_url <api_url>
+//	    email_address <email_address>
+//	    password <password>
+//	}
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
-		if d.NextArg() {
-			p.Provider.APIToken = d.Val()
-		}
 		if d.NextArg() {
 			return d.ArgErr()
 		}
 		for nesting := d.Nesting(); d.NextBlock(nesting); {
 			switch d.Val() {
-			case "api_token":
-				if p.Provider.APIToken != "" {
-					return d.Err("API token already set")
+			case "api_url":
+				if p.Provider.APIURL != "" {
+					return d.Err("API URL already set")
 				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.APIURL = d.Val()
 				if d.NextArg() {
-					p.Provider.APIToken = d.Val()
+					return d.ArgErr()
 				}
+			case "email_address":
+				if p.Provider.EmailAddress != "" {
+					return d.Err("email address already set")
+				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.EmailAddress = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "password":
+				if p.Provider.Password != "" {
+					return d.Err("password already set")
+				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.Password = d.Val()
 				if d.NextArg() {
 					return d.ArgErr()
 				}
@@ -63,8 +81,14 @@ func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 			}
 		}
 	}
-	if p.Provider.APIToken == "" {
-		return d.Err("missing API token")
+	if p.Provider.APIURL == "" {
+		return d.Err("missing API URL")
+	}
+	if p.Provider.EmailAddress == "" {
+		return d.Err("missing email address")
+	}
+	if p.Provider.Password == "" {
+		return d.Err("missing password")
 	}
 	return nil
 }
